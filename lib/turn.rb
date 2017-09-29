df546be3410408f7945ec84383a2cc76d5d4af3b
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-29 14:20:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,11 @@
+def display_board(board="")
+  rows = [" #{board[0]} | #{board[1]} | #{board[2]} ",\
+  " #{board[3]} | #{board[4]} | #{board[5]} ",\
+  " #{board[6]} | #{board[7]} | #{board[8]} "]
+  puts "#{rows[0]}\n-----------\n#{rows[1]}\n-----------\n#{rows[2]}"
+
+end
+
 def valid_move?(board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil && index.between?(0, 8)
     return true
@@ -7,10 +15,13 @@ def valid_move?(board, index)
   end
 end
 
-def display_board(board="")
-  rows = [" #{board[0]} | #{board[1]} | #{board[2]} ",\
-  " #{board[3]} | #{board[4]} | #{board[5]} ",\
-  " #{board[6]} | #{board[7]} | #{board[8]} "]
-  puts "#{rows[0]}\n-----------\n#{rows[1]}\n-----------\n#{rows[2]}"
+def input_to_index(input="-1")
+  input=(input.to_i) - 1;
 
 end
+
+def move(board, index, value="X")
+  return board[index]=value
+end
+
+
