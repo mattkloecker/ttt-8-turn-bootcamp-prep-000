fca4b3f4d18cc2e47a48f2cb7e8fb5d3c825c83e
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-29 15:33:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,7 +27,7 @@ def turn(board)
       until input.between?(1, 9)
         puts "Please enter 1-9:"
         input=gets.strip;
-        input= - 1;
+        input-1;
         input=(input.to_i);
         input_to_index(input);
     end
