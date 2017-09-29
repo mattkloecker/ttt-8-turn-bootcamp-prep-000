16102a1be3d0da6ec8936e1e553705bb3f287abd
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-29 14:18:44 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+def valid_move?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil && index.between?(0, 8)
+    return true
+  end
+  if board[index] == "X" || board[index] == "O"
+    return false
+  end
+end
+
+def display_board(board="")
+  rows = [" #{board[0]} | #{board[1]} | #{board[2]} ",\
+  " #{board[3]} | #{board[4]} | #{board[5]} ",\
+  " #{board[6]} | #{board[7]} | #{board[8]} "]
+  puts "#{rows[0]}\n-----------\n#{rows[1]}\n-----------\n#{rows[2]}"
+
+end
